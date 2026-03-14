c592be6e961141e13c94a2fcbed59978e0ec2c01
mkcheriyan997 <mkcheriyan997@gmail.com>
2026-03-14 17:17:43 +0530

    Final Update: 150+ Life Events, Enhanced Memory Matrix IQ Game, and Action Details

--- OOPS/LifeLoadSimulator/lifeload-server/src/main/java/com/lifeload/server/repository/AchievementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lifeload.server.repository;
-
-import com.lifeload.server.entity.Achievement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AchievementRepository extends JpaRepository<Achievement, String> {
-}
--- OOPS/LifeLoadSimulator/lifeload-server/src/main/java/com/lifeload/server/repository/TraitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lifeload.server.repository;
-
-import com.lifeload.server.entity.Trait;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TraitRepository extends JpaRepository<Trait, String> {
-}
