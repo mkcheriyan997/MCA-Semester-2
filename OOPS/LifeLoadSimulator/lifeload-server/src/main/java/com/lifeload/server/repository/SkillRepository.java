0b9621996612b0fa90a5beb898753802a6551b0c
mkcheriyan997 <mkcheriyan997@gmail.com>
2026-03-14 22:19:06 +0530

    Move LifeLoadSimulator project to a dedicated 'project' folder

--- OOPS/LifeLoadSimulator/lifeload-server/src/main/java/com/lifeload/server/repository/SkillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lifeload.server.repository;
-
-import com.lifeload.server.entity.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkillRepository extends JpaRepository<Skill, String> {
-}
